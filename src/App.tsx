98715efc30b9f4b3687e153688c192360f4812c6
Xin Yu <chuaxc@unimelb.edu.au>
2023-07-10 14:58:26 +1000

    Added Navbar and basic Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import Navbar from "./Navbar";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [rows, setRows] = useState([]);
+  const [columns, setColumns] = useState(["", ""]);
+  return (
+    <>
+      <main className="container mx-auto">
+        {/* Navbar */}
+        <Navbar />
+        {/* Sidebar */}
+
+        {/* Table */}
+        <div className="overflow-x-auto">
+          <table className="table table-xs">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Date</th>
+                <th>Amount</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr></tr>
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </>
+  );
 }
