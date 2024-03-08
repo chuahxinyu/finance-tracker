2a5bb164787806fd8104d40864c465461833f4bc
Xin Yu <chuaxc@unimelb.edu.au>
2024-03-08 18:36:46 +1100

    Add DaisyUI and File Input

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <input
+        type="file"
+        className="file-input file-input-bordered w-full max-w-xs"
+      />
     </div>
   );
 }
